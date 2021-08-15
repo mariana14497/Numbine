a6b261639893c2df275f1e45a47e36855946e734
Eyas Ziedan <66919391+EyasZ@users.noreply.github.com>
2021-08-15 14:01:26 +0300

    added button class
    first try

--- Numbine-master/src/app/demo/dell/generalScreens/log-in/log-in.component.ts
+++ Numbine-master/src/app/demo/dell/generalScreens/log-in/log-in.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ButtonClass} from 'src/app/demo/enums/myenum.enum'
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
 
+  buttonClass = ButtonClass;
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
