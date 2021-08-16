222f6afb7d6be79ce0af6d2923967a7a9c451a80
Hadi <hadidanial97@gmail.com>
2021-08-16 12:31:18 +0300

    Configurator Routing

--- Numbine-master/src/app/demo/dell/configurator/configurator-routing.module.ts
+++ Numbine-master/src/app/demo/dell/configurator/configurator-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductSelectorComponent } from './components/product-selector/product-selector.component';
+import { ProductReleaseMappingComponent } from './pages/product-release-mapping/product-release-mapping.component';
+import { TestConfigrationsEditNewComponent } from './pages/test-configrations-edit-new/test-configrations-edit-new.component';
+import { TestConfigurationComponent } from './pages/test-configuration/test-configuration.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: ProductSelectorComponent
+    path: 'releaseMapping',
+    component: ProductReleaseMappingComponent
+  },
+  {
+    path: 'testConfig',
+    component: TestConfigurationComponent
+  },
+  {
+    path: 'testConfigEdit',
+    component: TestConfigrationsEditNewComponent
   }
 ];
 
