3e12d4ebb9962107d3038302fda2a0c37233f609
NourAbdi <Dr.nour.3abdi@gmail.com>
2021-08-16 11:41:49 +0300

    adding pages

--- Numbine-master/src/app/demo/dell/configurator/configurator.module.ts
+++ Numbine-master/src/app/demo/dell/configurator/configurator.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConfiguratorRoutingModule } from './configurator-routing.module';
+import { ReleasesComponent } from './pages/releases/releases.component';
+import { UpdateReleaseComponent } from './pages/update-release/update-release.component';
+import { NewReleaseComponent } from './pages/new-release/new-release.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ReleasesComponent, UpdateReleaseComponent, NewReleaseComponent],
   imports: [
     CommonModule,
     ConfiguratorRoutingModule
