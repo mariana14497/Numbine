06e59943ba6b7ddd41fc49d4dca64e45d5613c22
Hadi <hadidanial97@gmail.com>
2021-08-18 00:09:34 +0300

    Update product.service.ts
    Renamed products

--- Numbine-master/src/app/demo/dell/services/ProductService/product.service.ts
+++ Numbine-master/src/app/demo/dell/services/ProductService/product.service.ts
@@ -19,8 +19,8 @@ export class ProductService {
             const release1 = new Release(1, "A", false, "1.0", "78/9/900");
             const release2 = new Release(2, "B", false, "1.2", "2/2/2010");
             const releases: Release[] = [release1, release2];
-            const product1 = new Product(1, "Apple", true, releases);
-            const product2 = new Product(2, "Car", true, releases);
+            const product1 = new Product(1, "Dell PC", true, releases);
+            const product2 = new Product(2, "Dell Laptop", true, releases);
             const products: Product[] = [product1, product2];
             observer.next(products);
           }, 500);
