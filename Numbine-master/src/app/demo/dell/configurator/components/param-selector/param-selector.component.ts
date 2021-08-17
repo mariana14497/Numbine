c9b9aedb7532e83a599b4c82e149674b588ba82e
NourAbdi <Dr.nour.3abdi@gmail.com>
2021-08-17 13:13:11 +0300

    working with param component

---
+++ Numbine-master/src/app/demo/dell/configurator/components/param-selector/param-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-param-selector',
+  templateUrl: './param-selector.component.html',
+  styleUrls: ['./param-selector.component.scss']
+})
+export class ParamSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
