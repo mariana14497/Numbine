04c43c526942872f4fd39b7066ef527e5cec2ad6
tsofen50 <tsofen50@gmail.com>
2021-08-17 20:49:25 +0300

    changes

--- Numbine-master/src/app/app.module.ts
+++ Numbine-master/src/app/app.module.ts
@@ -55,7 +55,8 @@ import { UpdateReleaseComponent } from './demo/dell/configurator/pages/update-re
 import { NumbineTableComponent } from './demo/dell/general-components/numbine-table/numbine-table.component';
 import { AppLoadingComponent } from './demo/dell/general-components/app-loading/app-loading.component';
 import { NumbineActionsTableComponent } from './demo/dell/general-components/numbine-actions-table/numbine-actions-table.component'
-import { ForgotPassComponent } from './demo/dell/generalScreens/log-in/forgot-pass/forgot-pass.component'
+import { ForgotPassComponent } from './demo/dell/generalScreens/log-in/forgot-pass/forgot-pass.component';
+import { AddUserPageComponent } from './demo/dell/Admin/pages/add-user-page/add-user-page/add-user-page.component'
 @NgModule({
   declarations: [
     NewReleaseComponent,
@@ -104,7 +105,8 @@ import { ForgotPassComponent } from './demo/dell/generalScreens/log-in/forgot-pa
     AccessPageComponent,
     AppLoadingComponent,
     NumbineActionsTableComponent,
-    ForgotPassComponent
+    ForgotPassComponent,
+    AddUserPageComponent
   ],
   imports: [
     BrowserModule,
