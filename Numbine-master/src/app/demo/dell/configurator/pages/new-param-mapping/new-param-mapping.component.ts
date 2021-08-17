e2e51700f463f5ebb8a2697e221597e12b513b0d
NourAbdi <Dr.nour.3abdi@gmail.com>
2021-08-17 16:02:57 +0300

    adding new params

--- Numbine-master/src/app/demo/dell/configurator/pages/new-param-mapping/new-param-mapping.component.ts
+++ Numbine-master/src/app/demo/dell/configurator/pages/new-param-mapping/new-param-mapping.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Parameter } from 'src/app/demo/dell/models/Parameter'
+import { ParameterService } from 'src/app/demo/dell/services/ParameterService/parameter.service';
+import { DataTable } from '../../../models/DataTable';
 
 @Component({
   selector: 'app-new-param-mapping',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewParamMappingComponent implements OnInit {
 
-  constructor() { }
+  title = ['Value','Start Release','End Release'];
+  rows = [[1,'"<app-icon type="edit"></app-icon>"',2],[3,'<app-icon type="edit"></app-icon>',4]];
+  dataTable = new DataTable();
+  
+  public parameters : Parameter[] = [];
+
+  constructor(private parameterService : ParameterService) { }
 
   ngOnInit(): void {
+    this.dataTable.rows=this.rows;
+    this.dataTable.titles=this.title;
+    
+    let obs = this.parameterService.getDummyParameters();
+    obs.subscribe(parameters => {
+      this.parameters = parameters;
+      // Add loading?
+    }, error =>
+    {
+      alert("Error in loading parameters, new-param-mapping.component.ts");
+    });
   }
 
 }
+
