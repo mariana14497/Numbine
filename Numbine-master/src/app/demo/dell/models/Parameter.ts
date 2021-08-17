ebc9f66d0fa0ef29af300ddd2f5c5bdcf23f55e4
IbrahemChahine <ibrahem@Ibrahem.localdomain>
2021-08-17 12:41:19 +0300

    added value[] and product[] to Parameter

--- Numbine-master/src/app/demo/dell/models/Parameter.ts
+++ Numbine-master/src/app/demo/dell/models/Parameter.ts
@@ -1,6 +1,9 @@
+import { Value} from './Value';
+import { Product} from './Product';
+
 export class Parameter
 {
-    public constructor(public id: number, public parameterName: string,public description: string)
+    public constructor(public id: number, public parameterName: string,public description: string, values: Value[], products : Product[])
     {
 
     }
